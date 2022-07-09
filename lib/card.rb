87f1b98e2b05316b8cfc03875f0f8fb97c013538
Jason Noble <jason.noble@itron.com>
2022-07-09 12:20:45 -0600

    card_spec is passing

---
+++ lib/card.rb
@@ -0,0 +1,9 @@
+class Card
+  attr_reader :category, :answer, :question
+
+  def initialize(question, answer, category)
+    @question = question
+    @answer = answer
+    @category = category
+  end
+end
