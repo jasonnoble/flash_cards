099b02b5c6c0ed799b8570fc4cfd41769fe4a3e3
Jason Noble <jason.noble@itron.com>
2022-07-09 12:29:20 -0600

    Turn#guess passing

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,16 +2,17 @@ require './lib/turn'
 require './lib/card'
 
 RSpec.describe Turn do
+  let(:card) { Card.new("What is the capital of Alaska?", "Juneau", :Geography) }
+  let(:turn) { Turn.new("Juneau", card) }
   describe '#new(guess, card)' do
     it 'creates a Turn' do
-      card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-      turn = Turn.new("Juneau", card)
-
       expect(turn).to be_a(Turn)
     end
   end
   describe '#guess' do
-    it 'returns the guess'
+    it 'returns the guess' do
+      expect(turn.guess).to eq('Juneau')
+    end
   end
   describe '#card' do
     it 'returns the card'
